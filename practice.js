f9a8b53aa95c4054684c6555b076cf0d4d8c3d8e
Jared Hensley <jahensle85@me.com>
2015-10-27 13:15:27 -0500

    finished

--- practice.js
+++ practice.js
@@ -1,29 +1,45 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
-  // 1) What is the purpose of the 'this keyword'?
+// 1) What is the purpose of the 'this keyword'?
+/*the this keyword is used as a reference to the object that contains the invoked function.  this creates a reference a that stores that value of the object and also allows you to point to that object without calling it by name, directly. */
+//Answer
 
-      //Answer
+// 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
-  // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
+/*will refer to the global object if not defined within a function that uses strict or is within an object, that is within the window object.  the this reference can be altered with methods such as .call, .apply and .bind. */
 
-      //Answer
+//Answer
 
-  // 3) What is the difference between call and apply?
+// 3) What is the difference between call and apply?
 
-      //Answer
+/*applys parameter argument is a single array like object, while call uses individual arguments.*/
 
-  // 4) What does .bind do?
+//Answer
 
-      //Answer
+// 4) What does .bind do?
+
+/*.bind creates a new copy and lets you curry the function with preset parameters.  */
+
+//Answer
 
 
 //Next Problem
 
 //Create an object called user which has the following properties.
-  //username --> which is a string
-  //email --> which is a string
-  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
+//username --> which is a string
+//email --> which is a string
+//getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+//Code Here
+
+var user = {
+  username: 'jared',
+  email: 'test@123.com',
+  getUsername: function () {
+    return this.username;
+  }
+};
+
+console.log(user.getUsername());
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
@@ -33,7 +49,22 @@
 
 // Write the function definitions which will make the following function invocations function properly.
 
-  //Function Invocations Here
+//Function Invocations Here
+
+function Car(company, model, year) {
+  this.company = company;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  moveC
+
+
+}
+
+Car.prototype.moveCar = function () {
+  this.move = this.move + 10;
+};
+
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -47,13 +78,16 @@ mustang.moveCar(); //increments mustang' move property by 10. Returns the new mo
 
 //Continuation of previous problem
 
-var getYear = function(){
+var getYear = function () {
   return this.year;
 };
 
+console.log(getYear.call(prius));
+console.log(getYear.call(mustang));
+
 //Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
 
-  //Code Here
+//Code Here
 
 
 
@@ -67,7 +101,7 @@ var user = {
   email: 'iliketurtles@gmail.com'
 };
 
-var getUsername = function(){
+var getUsername = function () {
   console.log(this.username);
 };
 
@@ -75,10 +109,36 @@ setTimeout(getUsername, 5000);
 
 //Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
 
-  //Answer Here
+//Answer Here this will point to the window object and will be undefined
 
-//In the example above, what is the 'this keyword' bound to when getUsername runs?
+//In the example above, what is the 'this keyword' bound to when getUsername runs? window object
 
-  //Answer Here
+//Answer Here
 
 //Fix the setTimeout invocation so that the user object will be the focal object when getUsername is ran.
+
+var user = {
+  username: 'iliketurtles',
+  age: 13,
+  email: 'iliketurtles@gmail.com',
+  /* setTimeOut: function() {setTimeout.call(this, getUsername, 5000);}*/
+  getUsername: function (last) {
+
+    console.log(this.username);
+  }
+};
+
+
+setTimeout(user.getUsername.bind(user), 5000, 'turner');
+/*setTimeout.call(user, getUsername, 5000;)*/
+
+
+//example of currying with bind 
+
+
+function myMathfunc(x, y) {
+  return x * y;
+}
+
+var double = myMathfunc.bind(myMathfunc, 2);
+double(5); // 10
